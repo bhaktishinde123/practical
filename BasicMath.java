ab1fa96532fb720815def801bedeeb59535cb6da
bhaktishinde123 <153122922+bhaktishinde123@users.noreply.github.com>
2024-01-25 20:33:58 +0530

    Add files via upload

---
+++ BasicMath.java
@@ -0,0 +1,22 @@
+class BasicMath
+{
+public static void main(String args[])
+{
+System.out.println("integer arithematic");
+int a = 1+1;
+int b = a*3;
+int c = b/4;
+int d = c-a;
+int e = -d;
+System.out.println("a="+a +" " +"b="+b +" " +"c=" +c +"  "+"d=" +d +" "+"e= " +e);
+
+
+System.out.println("\n Floating point Arithmetic");
+double da = 1+1;
+double db = da*3;
+double dc = db/4;
+double dd = dc-a;
+double de = -dd;
+System.out.println("da = "+da +"  "+"db = "+db +" "+ "dc = "+dc +"dd =" +dd + " " +"de=" +de);
+}
+}
