a629ec00953f16fe7331bf847eb42427d48f50fb
bhaktishinde123 <153122922+bhaktishinde123@users.noreply.github.com>
2024-01-25 20:37:20 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,18 @@
+class Ternary
+{
+	public static void main(String args[])
+	{
+		int i,k;
+		i = 10;
+		k = i < 0 ? -i : i;
+		System.out.print("absolute value 		of ");
+		System.out.println(i + "is" +k);
+		
+		i = -10;
+		k = i < 0 ? -i : i;
+		System.out.print("absolute value 		of ");
+		System.out.println(i + "is" +k);
+		
+
+	}
+}
