cb03b7f97d19471852b3765b0ac2b04a62d845a4
bhaktishinde123 <153122922+bhaktishinde123@users.noreply.github.com>
2024-01-25 20:35:05 +0530

    Add files via upload

---
+++ IncDec.java
@@ -0,0 +1,15 @@
+class IncDec
+{
+public static void main(String args[])
+{
+int a,b,c,d;
+a = 1;
+b = 2;
+c = ++b;
+d = a++;
+c++;
+System.out.println("a="+a +"  "+"b="+b +" "+"c="
++c +" " +"d= "+d);
+
+}
+}
