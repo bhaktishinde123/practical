ae006cdeae255269697c717e408e6f39603d72f3
bhaktishinde123 <153122922+bhaktishinde123@users.noreply.github.com>
2024-01-25 20:32:48 +0530

    Add files via upload

---
+++ scope.java
@@ -0,0 +1,19 @@
+class scope
+{
+public static void main(String args[])
+{
+int x;
+x =10;
+//int y =10;
+if(x==10)
+{
+int y =20;
+System.out.println("x and y:" +x +" "+y);
+x = y*2; 
+}
+int y =100;
+System.out.println("y is"+y);
+System.out.println("x ix"+x);
+
+}
+}
